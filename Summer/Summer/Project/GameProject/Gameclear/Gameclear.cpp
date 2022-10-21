313abdca1e9a6cd138e779547ff886f1709ed5dc
Haziki88 <22051913@d.anabuki-college.net>
2022-10-21 12:10:06 +0900

    a

--- Summer/Summer/Project/GameProject/Gameclear/Gameclear.cpp
+++ Summer/Summer/Project/GameProject/Gameclear/Gameclear.cpp
@@ -2,6 +2,7 @@
 
 Gameclear::Gameclear():Base(eType_Scene2)
 {
+
 }
 
 Gameclear::~Gameclear()
@@ -10,6 +11,10 @@ Gameclear::~Gameclear()
 
 void Gameclear::Update()
 {
+	//ボタン１でゲームオーバー破棄
+	if (PUSH(CInput::eButton1)) {
+		m_kill = true;
+	}
 }
 
 void Gameclear::Draw()
